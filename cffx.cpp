de5eb453254596696955403268dc6300e1ba0993
cjwddtc <cjwddtc@gmail.com>
2015-10-05 05:10:45 +0000

    add git ignore

--- cffx.cpp
+++ cffx.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-
+template <class state_type,class code_tpye>
 class print_
 {
 public:
-    void operator()(std::basic_string<code_type> a,state b)
+    void operator()(std::basic_string<code_tpye> a,state_type b)
     {
         std::cout << a << "\n" << b << std::endl;
     }
